f66d1e895f7100bdc76f5ac1e426f298a9267039
MafraJessyca <mafrajessyca@gmail.com>
2026-08-23 12:44:46 -0300

    Update moeda.java

--- src/cofre/moeda.java
+++ src/cofre/moeda.java
@@ -1,1 +1,16 @@
+package cofre;
+ // superClasse
+abstract class moeda {
+	protected double valor;
 
+	public moeda(double valor) {
+		this.valor = valor; 
+	}
+	
+	//polimorFismo 
+	public abstract double converter();
+	public double getValor() {
+		return valor;
+	}
+	
+}
