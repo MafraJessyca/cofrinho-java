dcc62ec43958a4c04ed327c77ddfc8cc8614522b
MafraJessyca <mafrajessyca@gmail.com>
2026-08-23 12:48:17 -0300

    Update real.java

--- src/cofre/real.java
+++ src/cofre/real.java
@@ -1,1 +1,16 @@
+package cofre;
 
+public class real extends moeda {
+	
+	public real(double valor) {
+		super (valor);
+	}
+	public double converter() {
+		return valor;
+		// NaoNecessarioConverter
+	}
+
+}
+ 
+
+	
