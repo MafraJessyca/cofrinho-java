e2ce796741c56287f12b8b144b72509ef5d0f831
MafraJessyca <mafrajessyca@gmail.com>
2026-08-23 12:47:12 -0300

    Update dolar.java

--- src/cofre/dolar.java
+++ src/cofre/dolar.java
@@ -1,1 +1,13 @@
+package cofre;
+//heRanca
+public class dolar extends moeda {
 
+	public dolar(double valor) {
+		super (valor);	
+	}
+	public double converter() {
+		return valor * 5; 
+		
+	}
+
+}
